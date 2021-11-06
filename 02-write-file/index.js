3e0172a0f53abaf6f488e0538420caeff1dfa349
Kanat Juzbayev <kanat.juzbayev@gmail.com>
2021-11-06 23:13:16 +0600

    feat: add functions 01-read-file and 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+console.log('Enter any texts:');
+
+fs.createWriteStream(path.join(__dirname, 'text.txt'));
+
+process.stdin.on('data', data => {
+  fs.writeFile(
+    path.join(__dirname, 'text.txt'),
+    data,
+    { flag: 'a+' },
+    err => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+    //file written successfully
+    });
+
+  console.log(`Added: ${data.toString()}`);
+});
+
+process.on('SIGINT', () => {
+  console.log('Exit. Goodbye, my friend! ;)');
+  process.exit();
+});
